06ffe925ef201e1eecdede4dd8398d643d757329
Natasha <natidrovo@gmail.com>
2019-09-23 22:51:42 +0200

    Falta js

---
+++ elements.js/main.js
@@ -0,0 +1,8 @@
+$(".menu-toggle").on('click', function() {
+    $(this).toggleClass("on");
+    $('.menu-section').toggleClass("on");
+    $("nav ul").toggleClass('hidden');
+  });
+  document.querySelector("body > div.section.about-me > div > p > a:nth-child(11)")
+  document.querySelector("body > div.cards > div > a:nth-child(2)")
+  document.querySelector("body > div.cards > div")
